fffab869d1b583461c1d029b1747c4a349ad9f20
MERYX-bh <jm_baha@esi.dz>
2022-06-30 00:18:30 +0200

    style app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+        <div className="blurblue" style={{top: '-18%', right: '0'}}></div>
+        <div className="blurpurple" style={{top: '36%', left: '-8rem'}}></div>
     </div>
   );
 }
