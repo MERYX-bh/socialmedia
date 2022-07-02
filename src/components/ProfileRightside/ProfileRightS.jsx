f44ace9f74aade1cf95004df58761bbca1c65e84
MERYX-bh <jm_baha@esi.dz>
2022-07-02 07:45:00 +0200

    Modals + login ,signup(front)

--- src/components/ProfileRightside/ProfileRightS.jsx
+++ src/components/ProfileRightside/ProfileRightS.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NotifBar from '../NotifBar/NotifBar';
 import TrendsCard from '../TrendsCard/TrendsCard';
 import './ProfileRightS.css'
+import ShareModal from '../ShareModal/ShareModal';
+
 
 const ProfileRightS = () => {
+  const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="ProfileRight">
         <NotifBar/>
         <TrendsCard/>
-        <button>Share</button>
+        <button onClick={() => setModalOpened(true)}>Share</button>
+        <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   )
 }
--- src/components/YourInfo/YourInfo.jsx
+++ src/components/YourInfo/YourInfo.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './YourInfo.css';
 import {UilPen} from '@iconscout/react-unicons';
+import ProfileModal from '../ProfileModal/ProfileModal';
 
 const YourInfo = () => {
   const [modalOpened, setModalOpened] = useState(false);
@@ -13,6 +14,12 @@ const YourInfo = () => {
          height="1.2rem"
          onClick={() => setModalOpened(true)}
         />
+         
+          <ProfileModal
+            modalOpened={modalOpened}
+            setModalOpened={setModalOpened}
+          />
+
         </div>
         <div className="info">
         <span>
